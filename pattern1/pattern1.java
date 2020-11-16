3aad6c2b08228cb6a38c386c245610b05c292a76
iamproneet <proneetofficial111@gmail.com>
2020-11-16 11:32:09 +0530

    1st pattern in java

---
+++ pattern1/pattern1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        // for(int row = 1; row <= n; row++){
+        //     for(int col = 1; col <= n; col++){
+        //         System.out.print("*	");
+        //     }
+        //     System.out.println();
+        // }
+        int row = 1, nst = 1;
+        while(row<=n){
+            for(int i = 1; i <= nst; i++){
+                System.out.print("*	");
+            }
+            System.out.println();
+            row++;
+            nst++;
+        }
+
+    }
+}
